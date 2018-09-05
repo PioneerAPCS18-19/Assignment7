e9926624d5e408711ca4451ca7bbb76c91c4192c
codytheking <codytheking93@gmail.com>
2018-09-05 13:00:08 -0700

    updating variables

---
+++ InClass/Test.java
@@ -0,0 +1,28 @@
+package InClass;
+
+
+/**
+ * Write a description of class Test here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Test
+{
+    public static void main(String[] args)
+    {
+        int num = 10;
+        num = num + 5;  // value of num is updated
+        num += 5;  // adds 5 to num
+        num *= 2;  // doubles num
+        
+        num = num + 1;
+        num += 1;
+        num++;  // increase (incremented) by 1
+        
+        
+        num = num - 1;
+        num -= 1;
+        num--;  // decreased (decremented) by 1
+    }
+}
